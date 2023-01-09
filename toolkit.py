49593ecf6056bafa74f3932bcb888dec20d6dbe2
telenhack <96855843+telenhackse@users.noreply.github.com>
2023-01-09 09:59:20 -0300

    Add files via upload

---
+++ toolkit.py
@@ -0,0 +1,182 @@
+import os
+import sys
+from colorama import init, Fore, Back, Style
+
+init()
+
+print(Fore.RED+"#####################################################################################")
+print(Fore.BLUE+"           toolKit By: telenhack\n\n\n")
+print(Fore.WHITE+"[1] PhoneInfoga: Marco de recopilación de información para números de teléfono.")
+print(Fore.WHITE+"[2] Lazymux-spanish: Lazymux un instalador de heramientas doblado.")
+print(Fore.WHITE+"[3] Vbug: Creador de virus.")
+print(Fore.WHITE+"[4] evilTrust: Herramienta ideal para el despliegue automatizado de un Rogue AP.")
+print(Fore.WHITE+"[5] btcAnalyzer: es una herramienta ideal para inspeccionar transacciones.")
+print(Fore.WHITE+"[6] spoofMe: Programa para el Spoofing de llamadas y mensajería, desde terminal.")
+print(Fore.WHITE+"[7] RemoteWA: Programa para gestionar WhatsApp desde terminal")
+print(Fore.WHITE+"[8] SETSMS: Herramienta para Spam de SMS echa en Python para linux/termux")
+print(Fore.WHITE+"[9] TheShark: herramienta para atacar un enlace phising arrojandole información falsa")
+print(Fore.WHITE+"[10] DDos-Web: Programa para ataque DDos-Web para páginas web ")
+print(Fore.WHITE+"[11] ip-info: Herramienta de uso facil para recolectar informacion sobre una IP")
+print(Fore.WHITE+"[12] CyberScan: kit de heramientas forenses de la red")
+print(Fore.WHITE+"[13] Xerxes: la herramienta DoS más poderosa ejemplo: ./xerxes www.fakesite.com 80")
+print(Fore.WHITE+"[14] CALL-BRUTAL: SPAMMER MASIVO DE LLAMADAS")
+print(Fore.WHITE+"[15] ASU: Facebook Hacking Tool")
+print(Fore.WHITE+"[16] sqlmap: inyección de SQL, toma control de la base de datos")
+print(Fore.WHITE+"[17] Sherlock: Busque cuentas de redes sociales por nombre de usuario")
+print(Fore.WHITE+"[18] hackingtool: herramienta de piratería todo en uno para piratas informáticos")
+print(Fore.YELLOW+"[0] salir\n")
+print(Fore.RED+"#####################################################################################")
+
+Toolkit = input("toolkit > set_option ")
+
+while Toolkit != 0:
+    if Toolkit == 00:
+        sys.exit()
+    elif Toolkit == 00:
+        sys.exit()
+    if Toolkit == 1:
+         os.system("apt update -y")
+         os.system("apt upgrade -y")
+         os.system("git clone https://github.com/la-deep-web/Phoneinfoga")
+    elif Toolkit == 1:
+         os.system("apt update -y")
+         os.system("apt upgrade -y")
+         os.system("git clone https://github.com/la-deep-web/Phoneinfoga")
+    if Toolkit == 2:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/telenhackse/lazymux-spanish")
+    elif Toolkit == 2:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/telenhackse/lazymux-spanish")
+    if Toolkit == 3:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/Gameye98/vbug")
+    elif Toolkit == 3:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/Gameye98/vbug")
+    if Toolkit == 4:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/evilTrust")
+    elif Toolkit == 4:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/evilTrust")
+    if Toolkit == 5:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/btcAnalyzer")
+    elif Toolkit == 5:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/btcAnalyzer")
+    if Toolkit == 6:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/spoofMe")
+    elif Toolkit == 6:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/spoofMe")
+    if Toolkit == 7:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/RemoteWA")
+    elif Toolkit == 7:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/s4vitar/RemoteWA")
+    if Toolkit == 8:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/Darkmux/SETSMS")
+    elif Toolkit == 8:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/Darkmux/SETSMS")
+    if Toolkit == 9:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("https://github.com/RIP-Network/TheShark")
+    elif Toolkit == 9:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("https://github.com/RIP-Network/TheShark")
+    if Toolkit == 10:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/RIP-Network/DDos-Web")
+    elif Toolkit == 10:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/RIP-Network/DDos-Web")
+    if Toolkit == 11:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/DarknetGhost/info-ip")
+    elif Toolkit == 11:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/DarknetGhost/info-ip")
+    if Toolkit == 12:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone git clone https://github.com/medbenali/CyberScan")
+    elif Toolkit == 12:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone git clone https://github.com/medbenali/CyberScan")
+    if Toolkit == 13:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system('apt install clang')
+        os.system("git clone git clone https://github.com/baraalmasri/xerxes")
+    elif Toolkit == 13:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system('apt install clang')
+        os.system("git clone git clone https://github.com/baraalmasri/xerxes")
+    if Toolkit == 14:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone git clone https://github.com/LOoLzeC/CALL-BRUTAL")
+    elif Toolkit == 14:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone git clone https://github.com/LOoLzeC/CALL-BRUTAL")
+    if Toolkit == 15:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/LOoLzeC/ASU")
+    elif Toolkit == 15:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/LOoLzeC/ASU")
+    if Toolkit == 16:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/sqlmapproject/sqlmap")
+    elif Toolkit == 16:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/sqlmapproject/sqlmap")
+    if Toolkit == 17:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/sherlock-project/sherlock")
+    elif Toolkit == 17:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/sherlock-project/sherlock")
+    if Toolkit == 18:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/Z4nzu/hackingtool")
+    elif Toolkit == 18:
+        os.system("apt update -y")
+        os.system("apt upgrade -y")
+        os.system("git clone https://github.com/Z4nzu/hackingtool")
